b20b57c5475b438d78fdae5a7e61e2a09ae86d82
Tiranderel <disou@bk.ru>
2020-05-14 13:21:11 +0300

    Настройка файла конфигурации для подключения к серверу

--- клиент/клиент.cpp
+++ клиент/клиент.cpp
@@ -2,14 +2,25 @@
 #include <conio.h>
 #include <stdio.h>
 #include <locale.h>
-
+#include <string>
+#include <fstream>
 #include <iostream>
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
 #pragma warning(disable: 4996)
+using namespace std;
 
 int main(int argc, char* argv[]) {
+
+	string fname = "adres.txt";
+	ifstream input(fname);
+	string address_s;
+	string port_s;
+	getline(input, address_s);
+
+	getline(input, port_s);
+
 	WSAData wsaData;
 	WORD DLLVersion = MAKEWORD(2, 1);
 	if (WSAStartup(DLLVersion, &wsaData) != 0) {
@@ -19,8 +30,8 @@ int main(int argc, char* argv[]) {
 	SOCKADDR_IN addr;
 	int razmerada = sizeof(addr);
 	//локалхост
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_port = htons(3487);
+	addr.sin_addr.s_addr = inet_addr(address_s.c_str());
+	addr.sin_port = htons(stoi(port_s));
 	addr.sin_family = AF_INET;
 
 	SOCKET podcluchenie = socket(AF_INET, SOCK_STREAM, NULL);
